611616f6d50f1b368f2c12848452311101b33d3b
Mahin Hasan <mahinmtrs@gmail.com>
2023-10-11 06:13:47 +0600

    added search functinality by phone name

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     // console.log(data.data);
@@ -9,10 +9,11 @@ const loadPhone = async () => {
 const displayPhones = phones => {
     // console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
     phones.forEach(phone => {
         // console.log(phone);
         const phoneCard = document.createElement('div');
-        phoneCard.classList = 'card w-96 bg-gray-100 shadow-xl';
+        phoneCard.classList = 'card p-4 bg-gray-100 shadow-xl';
         phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
@@ -27,8 +28,16 @@ const displayPhones = phones => {
     });
 }
 
+// Handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    loadPhone(searchText);
+}
 
 
 
 
-loadPhone();
+
+// loadPhone();
